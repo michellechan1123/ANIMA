b2b8d61b22af3b30cfd5da8ae798466b23575aba
Ohhun Kwon <ohhunkwon@hotmail.com>
2022-09-03 05:28:33 +0100

    harvard sentences can be recorded

--- gui/main.py
+++ gui/main.py
@@ -1,7 +1,10 @@
 from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivy.properties import StringProperty
+import time
 import random
+import pyaudio
+import wave
 from src.ANIMA import ANIMA
 from src.manage_audio import AudioManager
 
@@ -23,6 +26,10 @@ class MainApp(MDApp):
         self.theme_cls.theme_style = "Light"
         self.theme_cls.primary_palette = "LightBlue"
         self.anima = ANIMA()
+        self.audio_manager = AudioManager()
+        self.audio = pyaudio.PyAudio()
+        self.audio_stream = None
+        self.audio_frames = None
         return Builder.load_file("styles.kv")
 
     def next_sentence(self):
@@ -43,14 +50,38 @@ class MainApp(MDApp):
         except IndexError:
             print("please select a file")
 
-    def process_toggle_recording(self):
-        print("toggle state: " + self.root.ids.toggle_recording.state)
-        if self.root.ids.toggle_recording.state == "normal":
-            self.root.ids.toggle_recording.text = "Start Recording"
-            self.root.ids.toggle_recording.color = (1, 1, 1, 1)
-        else:
-            self.root.ids.toggle_recording.text = "Recording..."
-            self.root.ids.toggle_recording.color = (0, 0, 0, 1)
+    def play_selected(self, filename):
+        try:
+            self.audio_manager.play_audio('en', filename[0])
+        except IndexError:
+            print("please select a file")
+
+    def recording(self):
+        self.audio_stream = self.audio.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, frames_per_buffer=1024)
+        self.audio_frames = []
+
+        t_end = time.time() + 5
+        while time.time() < t_end:
+            data = self.audio_stream.read(1024)
+            self.audio_frames.append(data)
+
+    def start_recording(self):
+        print("Recording in progress...")
+        self.recording()
+
+    def exit_recording(self):
+        try:
+            print("Recording stopped.")
+            self.audio_stream.stop_stream()
+            self.audio_stream.close()
+            sound_file = wave.open(self.root.ids.input_filename_voice.text, "wb")
+            sound_file.setnchannels(1)
+            sound_file.setsampwidth(self.audio.get_sample_size(pyaudio.paInt16))
+            sound_file.setframerate(44100)
+            sound_file.writeframes(b''.join(self.audio_frames))
+            sound_file.close()
+        except AttributeError:
+            print("please enter a filename")
 
 
 if __name__ == '__main__':
